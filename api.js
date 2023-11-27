cf5816c19c84a1daa16faeda19d06586ffef6d5d
Fadhil1341 <121000999+Fadhil1341@users.noreply.github.com>
2023-11-27 15:07:51 +0800

    Add files via upload

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let mongoose = require('mongoose')
 const mma = require('./mma')
+let cors = require('cors')
 
 
 
@@ -10,6 +11,8 @@ let app = express()
 // configure express app to encode/decode 
 // JSON 
 app.use(express.json())
+// consfigure cors to allow ALL/selected incoming request
+app.use(cors())
 
 // connect to mongodb databasemongodb+srv://fadhilismail1341:<password>@airasia.uq749af.mongodb.net/
 let dbstring ="mongodb+srv://fadhilismail1341:Password131313@airasia.uq749af.mongodb.net/youtube"
@@ -37,7 +40,38 @@ app.get("/1.0/youtube/all", (request, response)=>{
             })
 })
 
+// add new video document to the database
+// POST http://localhost:1234/1.0/youtube/add
+app.post("/1.0/youtube/add", (request, response)=>{
+    console.log("POST request for //1.0/youtube/add")
+    console.log(request.body)
+    console.log(request.body.title)
+    console.log(request.body.vid)
+    console.log(request.body.likes)
+    console.log(request.body.dislikes)
 
+    // create instance of model -> mma.js
+    let mmaNew = new mma ({
+        title:request.body.title,
+        vid:request.body.vid,
+        likes:request.body.likes,
+        dislikes:request.body.dislikes
+
+    })
+    // save the model instance in database
+    mmaNew.save()
+            .then((data)=>{
+                response.send({
+                    "status":"success",
+                    "saved":data
+                })
+            })
+            .catch((error)=>{
+                response.send(error)
+            })
+
+
+} )
 
 
 
